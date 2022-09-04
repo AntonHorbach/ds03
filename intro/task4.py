5ed42e5e39cf414dc8d58e8b20bc5c759e277817
AntonHorbach <antos.horbach@gmail.com>
2022-09-04 16:27:05 +0300

    task5

--- intro/task4.py
+++ intro/task4.py
@@ -1,17 +1,11 @@
 import random
 import math
 
-
-def rand_vec(dim=3):
-    return [random.uniform(0, 100) for i in range(dim)]
-
-
-def vec_len(vec):
-    return math.sqrt(sum(i**2 for i in vec))
+from common.vectors import vec_len, rand_vec, vec_cos
 
 
 def cosine_similarity(f_vec, s_vec):
-    return sum(f * s for f, s in zip(f_vec, s_vec)) / (vec_len(f_vec) * vec_len(s_vec))
+    return vec_cos(f_vec, s_vec)
 
 
 def cosine_distance(f_vec, s_vec):
